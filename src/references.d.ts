621b67af8e6ce38912837dcc1be0954c37d79124
Federico Ricchiuto <fed.ricchiuto@gmail.com>
2020-10-25 19:11:11 +0100

    refactor(src): updated references.d.ts - use android-26 types

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,11 +1,5 @@
-/// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
-/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-26.d.ts" />
 
 /// <reference path="./platforms/ios/typings/objc!GoogleMapsBase.d.ts" />
 /// <reference path="./platforms/ios/typings/objc!GoogleMaps.d.ts" />
-
-/*
- To use a specific platform version definition:
- <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
- <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
- */
